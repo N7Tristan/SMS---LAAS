a823e12a560953a30e52e5f8ae0263630a78e6ac
N7Tristan <tristan.schultz.57@gmail.com>
2015-06-23 16:47:27 +0200

    Traitement pas mal du tout

--- app/device/labtool/traitement.cpp
+++ app/device/labtool/traitement.cpp
@@ -18,18 +18,17 @@ QVector<double>*  traitement(QVector<double>* s)
     double *data_buff = buffer->data();
 
 
+    double max_A = 0;
+    double min_A = 0;
 
-    double max_A = -1;
-    double min_A = 1;
-    double min1 = 1;
-    double min2 = 1;
-    double max1 = -1;
-    double max2 = -1;
+    double Gain = 0;
 
 
-    double cpt_max=0;
-    double cpt_min=0;
+    double *diffDataMod;
+    diffDataMod = new double[taille];
 
+    double *vectDiffDataMod;
+    vectDiffDataMod = new double[taille];
 
     double *frange;
     frange = new double[taille];
@@ -37,244 +36,153 @@ QVector<double>*  traitement(QVector<double>* s)
     double *reconstitution;
     reconstitution = new double[taille];
 
-
-    double sens = 0;
-    double sens_precedent = 0;
-
-    double seuil = 0.05;
-
-    double Amplitude = 0;
-    double Gain = 1;
-
-    double moy=0;
-
-    double mrf=0;
-    double C=0;
-
-    double *Cvect;
-    Cvect = new double[taille];
+    double *signal;
+    signal = new double[taille];
 
 
+
+    // //////////////////////////////////////////////////// //
+    // Detection du min et du max du signal pour normaliser //
+    // //////////////////////////////////////////////////// //
+
+    for (unsigned int i=1; i<taille-2; i++)
+    {
+        if(data[i] > data[i-1])
+        {
+            if(max_A < data[i])
+            {
+                max_A = data[i];
+            }
+        }
+
+        else if(data[i] < data[i-1])
+        {
+            if(min_A > data[i])
+            {
+                min_A = data[i];
+            }
+        }
+    }
+
+    Gain = 1/(max_A-min_A);
+
+    for (unsigned int i=1; i<taille-2; i++)
+    {
+
+        if(i>30)
+        {
+            for(int k = 0; k<30; k++)
+            {
+            data_mod[i] = Gain*data[i] + 0.0333 * Gain * data[k];
+            }
+        }
+
+        else
+            data_mod[i] = Gain * data[i];
+
+    }
+
+    // //////////////////////////////////////////////////// //
+    // //////////////////////////////////////////////////// //
+
+
+
+    unsigned int dt = 20;
+    double gainDiff = 2;
+
+    for (unsigned int i=1+dt; i<taille-(dt+1); i++)
+    {
+        diffDataMod[i] = data[i+dt]-data[i];
+
+
+        if(diffDataMod[i]>diffDataMod[i-dt] && diffDataMod[i]>diffDataMod[i+dt])
+        {
+            vectDiffDataMod[i] = gainDiff*diffDataMod[i];
+        }
+
+        else if(diffDataMod[i]<diffDataMod[i-dt] && diffDataMod[i]<diffDataMod[i+dt])
+        {
+            vectDiffDataMod[i] = gainDiff*diffDataMod[i];
+        }
+
+        else
+        {
+            vectDiffDataMod[i] = 0;
+        }
+    }
+
+
+    double seuil = 1;
+    int flag_down = 0;
+    int flag_up = 0;
 
 
     for (unsigned int i=1; i<taille-2; i++)
     {
-        data_mod[i] = Gain * data[i];
-
-        if(data_mod[i] > data_mod[i-1])
+        if(vectDiffDataMod[i]<(-1)*seuil && flag_up == 0)
         {
-            if(max_A < data_mod[i])
-            {
-                max_A = data_mod[i];
-            }
+            frange[i] = 1;
+            flag_up = 1;
         }
 
-        else if(data_mod[i] < data_mod[i-1])
+        else if(vectDiffDataMod[i]>seuil && flag_down == 0)
         {
-            if(min_A > data_mod[i])
-            {
-                min_A = data_mod[i];
-            }
-        }
-
-
-
-
-        if(i%6400 == 0)
-        {
-            Amplitude=(max_A-min_A)/Gain;
-            Gain=1/Amplitude;
-
-            mrf = min1 - min2;
-
-            C=2.9*mrf+1.46;
-
-            max_A=-1;
-            min_A=1;
-
-            min1=1;
-            min2=1;
-
-            max1=-1;
-            max2=-1;
-        }
-
-        Cvect[i]=C;
-
-        if(data_mod[i]-data_mod[i-1] > seuil)
-        {
-            frange[i]=-1;
-
-            if(min2>data_mod[i-1])
-                min2=data_mod[i-1];
-        }
-
-        else if(data_mod[i]-data_mod[i-1] < (-1)*seuil)
-        {
-            frange[i]=1;
-
-            if(min1>data_mod[i])
-                min1=data_mod[i];
+            frange[i] = -1;
+            flag_down = 1;
         }
 
         else
+        {
+
+            if(vectDiffDataMod[i]>=(-1)*seuil && flag_up == 1)
+                flag_up = 0;
+
+            if(vectDiffDataMod[i]<=seuil && flag_down == 1)
+                flag_down = 0;
+
             frange[i]=0;
+        }
 
 
 
 
-        if(frange[i]==-1)
-            sens=-1;
-        else if(frange[i]==1)
-            sens=1;
+         //data_buff[i] = frange[i];
+     }
+
+    double moy = 0;
+
+
+    for (unsigned int i=1; i<taille-2; i++)
+    {
+        if(frange[i] == 1)
+        {
+            reconstitution[i]=reconstitution[i-1]+0.5;
+        }
+
+        else if(frange[i] == -1)
+        {
+            reconstitution[i]=reconstitution[i-1]-0.5;
+        }
+
         else
-            sens=sens_precedent;
-
-
-        if(frange[i-1]==-1 && frange[i]==0 && i > 6400)
-        {
-            cpt_min=cpt_min+1;
-            reconstitution[i]=cpt_max-cpt_min;
-        }
-        else if(frange[i-1]==1 && frange[i]==0 && i > 6400)
-        {
-            cpt_max=cpt_max+1;
-            reconstitution[i]=cpt_max-cpt_min;
-        }
-
-
-        else if(i > 6400)
             reconstitution[i]=reconstitution[i-1];
 
-        else
-            reconstitution[i]=0;
+        signal[i] = reconstitution[i] + data_mod[i];
 
-
-        moy = (moy + reconstitution[i])/i;
-
-        if(i>6400)
-            reconstitution[i]=reconstitution[i]-moy;
-
-
-        sens_precedent=sens;
-
-        data_buff[i] = reconstitution[i]+data_mod[i];
+        moy = moy + signal[i];
 
     }
 
 
-
-    /*for (unsigned int i=1; i<taille-2; i++)
+    for (unsigned int i=1; i<taille-2; i++)
     {
-        data_mod[i] = Gain * data[i];
-
-        if(data_mod[i] > data_mod[i-1])
-        {
-            if(max_A < data_mod[i])
-            {
-                max_A = data_mod[i];
-            }
-        }
-
-        else if(data_mod[i] < data_mod[i-1])
-        {
-            if(min_A > data_mod[i])
-            {
-                min_A = data_mod[i];
-            }
-        }
-
-
-        if(data_mod[i] - data_mod[i-1] > seuil)
-        {
-            frange[i]=-1;
-
-            if(min2>data_mod[i-1])
-                min2=data_mod[i-1];
-        }
-
-        else if(data_mod[i] - data_mod[i-1] < (-1)*seuil)
-        {
-            frange[i]=1;
-
-            if(min1>data_mod[i])
-                min1=data_mod[i];
-        }
-
-        else
-            frange[i]=0;
-
-
-
-        if(frange[i]==-1)
-            sens=-1;
-        else if(frange[i]==1)
-            sens=1;
-        else
-            sens=sens_precedent;
-
-
-        if(frange[i-1]==-1 && frange[i]==0 && i > 3200)
-        {
-            cpt_min=cpt_min+1;
-            reconstitution[i]=cpt_max-cpt_min;
-        }
-        else if(frange[i-1]==1 && frange[i]==0 && i > 3200)
-        {
-            cpt_max=cpt_max+1;
-            reconstitution[i]=cpt_max-cpt_min;
-        }
-
-        else if(i > 3200)
-            reconstitution[i]=reconstitution[i-1];
-
-        else
-            reconstitution[i]=0;
-
-
-        moy = (moy + reconstitution[i])/i;
-
-        reconstitution[i]=reconstitution[i]-moy;
-
-
-
-
-        sens_precedent=sens;
-
-        data_buff[i] = reconstitution[i];
-
+        data_buff[i] = signal[i] - moy/(taille-2);
     }
-
-
-
-
-    for (unsigned int i=0; i<taille; i++)
-    {
-        moy = moy + reconstitution[i];
-    }
-
-    moy = moy / taille;
-
-
-
-
-    for (unsigned int i=6401; i<taille; i++)
-    {
-        reconstitution[i]=reconstitution[i]-moy;
-        data_buff[i] = reconstitution[i]+data[i];
-
-
-    }*/
-
-
-
-
-
-    delete[] frange;
-    delete[] reconstitution;
 
 
     return buffer;
 
 }
+
+
 
